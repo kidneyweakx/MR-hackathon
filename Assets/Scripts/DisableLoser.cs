aa06ffc6cf8398ccd3dd726535017bb6532a7b2e
kidneyweakx <kidneyweakx@gmail.com>
2019-07-28 13:29:32 +0800

    demo version

--- Assets/Scripts/DisableLoser.cs
+++ Assets/Scripts/DisableLoser.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class DisableLoser : MonoBehaviour
 {
     public GameObject loser;
-    public GameObject MR;
+
     // Start is called before the first frame update
     void Start()
     {
         Destroy(loser, 8);
-        Resources.UnloadAsset(MR);
     }
 }
