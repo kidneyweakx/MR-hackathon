2517d2821b506c136b7f859252c6f7033114e3fd
kidneyweakx <kidneyweakx@gmail.com>
2019-07-28 12:49:06 +0800

    add 2D scene

---
+++ Assets/Scripts/DisableLoser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableLoser : MonoBehaviour
+{
+    public GameObject loser;
+    public GameObject MR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(loser, 8);
+        Resources.UnloadAsset(MR);
+    }
+}
